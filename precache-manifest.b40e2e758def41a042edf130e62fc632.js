de6f79f9a86d95c50770b85918de3bd36d7598c2
Snehal <isnehalr@gmail.com>
2020-03-19 17:37:21 +0530

    Updates

---
+++ precache-manifest.b40e2e758def41a042edf130e62fc632.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e73104f98ae4063cfce8d61c62c0692c",
+    "url": "/resume-app/index.html"
+  },
+  {
+    "revision": "c138f36095bf45c68789",
+    "url": "/resume-app/static/css/2.73bac6f1.chunk.css"
+  },
+  {
+    "revision": "e2829848b70e0dd51c3e",
+    "url": "/resume-app/static/css/main.6ac1ccfb.chunk.css"
+  },
+  {
+    "revision": "c138f36095bf45c68789",
+    "url": "/resume-app/static/js/2.b6c01da9.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/resume-app/static/js/2.b6c01da9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2829848b70e0dd51c3e",
+    "url": "/resume-app/static/js/main.f88738d5.chunk.js"
+  },
+  {
+    "revision": "0ece45edb2e7aaf51043",
+    "url": "/resume-app/static/js/runtime-main.ff93cd86.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/resume-app/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/resume-app/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/resume-app/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/resume-app/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/resume-app/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b87aa3145b2d967e42b6804f24e1f57a",
+    "url": "/resume-app/static/media/img.b87aa314.jpg"
+  }
+]);
